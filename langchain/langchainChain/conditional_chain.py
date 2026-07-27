e3c31e9177a4a13edeb6eb7f5a138ce316e3076c
kapilsingh09 <karan404katyura@gmail.com>
2026-07-27 20:15:02 +0530

    langchain chains topic complete

--- langchain/langchainChain/conditional_chain.py
+++ langchain/langchainChain/conditional_chain.py
@@ -1,47 +1,51 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableParallel, RunnableBranch, RunnableLambda
 from langchain_core.output_parsers import PydanticOutputParser
-from langchain.schema.runnable import RunnableBranch
 from pydantic import BaseModel, Field
 from typing import Literal
-import pandas as pd
-import io
-import os
 
 load_dotenv()
 
-os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
-
-model = ChatGoogleGenerativeAI(
-    model="gemini-3.6-flash",
-    temperature=0
-)
-
-
+model = ChatOpenAI()
 
 parser = StrOutputParser()
 
-
 class Feedback(BaseModel):
 
-    sentiment:Literal['positive','negative'] = Field(description='give the sentiment of the follwing feedback')
-
+    sentiment: Literal['positive', 'negative'] = Field(description='Give the sentiment of the feedback')
 
 parser2 = PydanticOutputParser(pydantic_object=Feedback)
+
 prompt1 = PromptTemplate(
-    template="classify the sentiment of the following feedback text into positive or negative \n {feedback} \n {format_instruction}",
+    template='Classify the sentiment of the following feedback text into postive or negative \n {feedback} \n {format_instruction}',
     input_variables=['feedback'],
     partial_variables={'format_instruction':parser2.get_format_instructions()}
 )
 
-classifier_chain = prompt1 | model | parser
+classifier_chain = prompt1 | model | parser2
 
-print(classifier_chain.invoke({"text":"this is a terriable mobile phone"}))
+prompt2 = PromptTemplate(
+    template='Write an appropriate response to this positive feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template='Write an appropriate response to this negative feedback \n {feedback}',
+    input_variables=['feedback']
+)
 
 branch_chain = RunnableBranch(
-    (),
-    (),
-    ()
+    (lambda x:x.sentiment == 'positive', prompt2 | model | parser),
+    (lambda x:x.sentiment == 'negative', prompt3 | model | parser),
+    RunnableLambda(lambda x: "could not find sentiment")
 )
+
+chain = classifier_chain | branch_chain
+
+print(chain.invoke({'feedback': 'This is a beautiful phone'}))
+
+chain.get_graph().print_ascii()
